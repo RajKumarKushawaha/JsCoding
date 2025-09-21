eed892a28f7d21fbf5a2f288103d18c636d677ec
RajKumarKushawaha <rajkumarkushwaha9155@gmail.com>
2025-09-20 23:20:54 -0500

    fibonacci series

---
+++ fibonacci_second_method.js
@@ -0,0 +1,10 @@
+function printFabonacci(n) {
+
+    let arr = [0, 1];
+    for (let i = 2; i < n; i++) {
+        arr.push(arr[i - 2] + arr[i - 1])
+    }
+    return arr;
+}
+
+console.log(printFabonacci(6))
